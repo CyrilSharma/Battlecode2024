2e677b303e5fbe06bf8b9ad5aee91f89247db822
CyrilSharma <sharma.cyril@gmail.com>
2024-01-17 23:17:26 -0500

    Revert "commiting this because it was kind of cool"
    This reverts commit 2ea9f20a86b2c0c186b1849111539cf0ecd028e9.

--- src/initialBot/NeighborLoader.py
+++ src/initialBot/NeighborLoader.py
@@ -5,11 +5,6 @@ FE_MASK_HEIGHT = 7
 VISION = 9
 CLASS_NAME = "NeighborLoader"
 BITS_PER_MASK = 63
-PIECES = [
-    "heal",
-    "attack"
-]
-MAX_LEVEL = 6
 
 class ClassPrinter:
     def __init__(self):
@@ -39,10 +34,10 @@ def printLoader():
         with cp:
             cp.print("this.rc = rc;")
         cp.print("}")
-        cp.print("public void load(NeighborTracker nt) throws GameActionException {")
+        cp.print("public void load(AttackMicro am) throws GameActionException {")
         with cp:
-            cp.print("load_allies(nt);")
-            cp.print("load_enemies(nt);")
+            cp.print("load_allies(am);")
+            cp.print("load_enemies(am);")
         cp.print("}")
         cp.print()
         load_allies()
@@ -51,12 +46,12 @@ def printLoader():
     cp.print("}")
 
 def load_allies():
-    cp.print("public void load_allies(NeighborTracker nt) throws GameActionException {")
+    cp.print("public void load_allies(AttackMicro am) throws GameActionException {")
     with cp: load_pieces(True)
     cp.print("}")
 
 def load_enemies():
-    cp.print("public void load_enemies(NeighborTracker nt) throws GameActionException {")
+    cp.print("public void load_enemies(AttackMicro am) throws GameActionException {")
     with cp: load_pieces(False)
     cp.print("}")
 
@@ -64,45 +59,27 @@ def load_pieces(friend):
     cp.print(f"int diff = {-(VISION//2)};")
     cp.print(f"int offset = rc.getLocation().translate(diff, diff).hashCode();")
     nmasks = (VISION * VISION + BITS_PER_MASK - 1) // BITS_PER_MASK
-    for piece in PIECES:
-        for level in range(0, MAX_LEVEL + 1):
-            for i in range(nmasks):
-                cp.print(f"long t_{piece}{level}_mask{i} = 0;")
+    for i in range(nmasks):
+        cp.print(f"long t_mask{i} = 0;")
     
     team = "rc.getTeam()" if friend else "rc.getTeam().opponent()"
     cp.print(f"RobotInfo[] robots = rc.senseNearbyRobots(-1, {team});")
     cp.print("for (int j = robots.length; j-- > 0; ) {")
-    with cp:
-        cp.print("RobotInfo r = robots[j];")
-        cp.print("switch (r.getAttackLevel()) {")
-        for j in range(0, MAX_LEVEL + 1):
-            with cp:
-                cp.print(f"case {j}:")
-                with cp: load_switch(f"t_attack{level}_mask")
-        cp.print("}")
-        cp.print("switch (r.getHealLevel()) {")
-        for j in range(0, MAX_LEVEL + 1):
-            with cp:
-                cp.print(f"case {j}:")
-                with cp: load_switch(f"t_heal{level}_mask")
-        cp.print("}")
+    with cp: load_switch(f"t_mask")
     cp.print("}")
     cp.print()
 
     prefix = "friend" if friend else "enemy"
-    for level in range(0, MAX_LEVEL + 1):
-        for piece in PIECES:
-            for i in range(nmasks):
-                cp.print(f"nt.{prefix}_{piece}_mask1[{level}] = t_{piece}{level}_mask{i};")
-                cp.print(f"nt.{prefix}_{piece}_mask0[{level}] = t_{piece}{level}_mask{i};")
+    cp.print(f"am.{prefix}_mask1 = t_mask1;")
+    cp.print(f"am.{prefix}_mask0 = t_mask0;")
 
-    if (friend): cp.print("nt.friends = robots;")
-    else:        cp.print("nt.enemies = robots;")
+    if (friend): cp.print("am.friends = robots;")
+    else:        cp.print("am.enemies = robots;")
 
 
 
 def load_switch(mask):
-    cp.print("switch (r.location.hashCode() - offset) {")
+    cp.print("switch (robots[j].location.hashCode() - offset) {")
     with cp:
         for idx in range(VISION * VISION):
             y = idx // 9; x = idx % 9;
